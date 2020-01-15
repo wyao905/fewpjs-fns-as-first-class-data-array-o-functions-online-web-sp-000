9f7f1f7103272fd9f2511df09fbf832a30e8e102
Warren Yao <warrenyca905@gmail.com>
2020-01-15 00:02:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-function wakeDog() {
+function wakeDog(dogName, dogBreed) {
   console.log("Wake Byron the poodle");
 }
-function leashDog() {
+function leashDog(dogName, dogBreed) {
   console.log("Leash Byron the poodle");
 }
-function walkDogPark() {
+function walkDogPark(dogName, dogBreed) {
   console.log("Walk to the park with Byron the poodle");
 }
-function frisbeeDog() {
+function frisbeeDog(dogName, dogBreed) {
   console.log("Throw the frisbee for Byron the poodle");
 }
-function walkDogHome() {
+function walkDogHome(dogName, dogBreed) {
   console.log("Walk home with Byron the poodle");
 }
-function unleashDog() {
-  
+function unleashDog(dogName, dogBreed) {
+  console.log("Unleash Byron the poodle");
 }
