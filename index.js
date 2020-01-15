c38543f9a113f22a20d404372d27c98a86f036e6
Warren Yao <warrenyca905@gmail.com>
2020-01-15 00:45:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 function wakeDog(dogName, dogBreed) {
-  str = `Wake ${dogName} the ${dogBreed}`;
+  let str = `Wake ${dogName} the ${dogBreed}`;
   console.log(str);
   return str;
 }
 function leashDog(dogName, dogBreed) {
-  str = `Leash ${dogName} the ${dogBreed}`;
+  let str = `Leash ${dogName} the ${dogBreed}`;
   console.log(str);
   return str;
 }
 function walkToPark(dogName, dogBreed) {
-  str = `Walk to the park with ${dogName} the ${dogBreed}`;
+  let str = `Walk to the park with ${dogName} the ${dogBreed}`;
   console.log(str);
   return str;
 }
 function throwFrisbee(dogName, dogBreed) {
-  str = `Throw the frisbee for ${dogName} the ${dogBreed}`;
+  let str = `Throw the frisbee for ${dogName} the ${dogBreed}`;
   console.log(str);
   return str;
 }
 function walkHome(dogName, dogBreed) {
-  str = `Walk home with ${dogName} the ${dogBreed}`;
+  let str = `Walk home with ${dogName} the ${dogBreed}`;
   console.log(str);
   return str;
 }
 function unleashDog(dogName, dogBreed) {
-  str = `Unleash ${dogName} the ${dogBreed}`;
+  let str = `Unleash ${dogName} the ${dogBreed}`;
   console.log(str);
   return str;
 }
