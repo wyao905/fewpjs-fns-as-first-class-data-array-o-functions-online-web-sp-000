8dd1d3da609f4fafe920494ee102cfcba298e695
Warren Yao <warrenyca905@gmail.com>
2020-01-15 00:01:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function leashDog() {
   console.log("Leash Byron the poodle");
 }
 function walkDogPark() {
-  
+  console.log("Walk to the park with Byron the poodle");
 }
 function frisbeeDog() {
   
